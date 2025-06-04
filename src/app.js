c62887706df1a5a69741497eb857129364080a03
mohitchouhan11 <mohitchouhan.one@gmail.com>
2025-06-04 22:06:52 +0530

    exploring get , post , delete

--- src/app.js
+++ src/app.js
@@ -2,9 +2,30 @@ const express = require('express');
 
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("Namaste from the dashboard!")
+// app.use("/",(req,res)=>{
+//     res.send("Namaste from the dashboard!")
+// })
+
+//This will only handle GET call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Mohit",lastname:"Chouhan"});
+});
+
+app.post("/user",async(req,res)=>{
+    console.log(req.body);
+    //saving data to DB
+    res.send("Data successfully saved to the database!");
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("Deleted successfully!!");
+});
+
+//This will match all the http method API calls to /test
+app.use("/hello/2",(req,res)=>{
+    res.send("Abracadarab !")
 })
+
 
 app.use("/hello",(req,res)=>{
     res.send("Hello Hello Hello!!")
@@ -13,6 +34,10 @@ app.use("/hello",(req,res)=>{
 app.use("/test",(req,res)=>{
     res.send("Hello from the Server!!")
 })
+
+// app.use("/",(req,res)=>{
+//     res.send("Namaste from the dashboard!")
+// })
 
 app.listen(3000, ()=>{
     console.log("Server is successfully listening on port 3000...");
